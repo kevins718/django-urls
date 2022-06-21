6d122a62bb39ca8a39674cd1d34b46fb5954cd50
Kevin Outta <outaskevin@gmail.com>
2022-06-21 09:01:47 +0300

    changes to the url with zuri-admin

--- o_tasky/o_tasky/settings.py
+++ o_tasky/o_tasky/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zqn@&vq!ur%n&j!vqgewik=xw69zx!r9n89u6z1^idyhmma4%q'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
